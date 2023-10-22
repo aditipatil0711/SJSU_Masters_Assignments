7925c1112d10a3c140b1a6a5b594d1025d81fb85
Addy <patiladiti752@gmail.com>
2023-10-22 06:12:16 -0700

    Added Assignment for Deloitte App

---
+++ CMPE273/Assignment-7/deloitte-chat-bot/gradio_ui.py
@@ -0,0 +1,25 @@
+import gradio as gr
+
+from untils import generate_response, set_user_query
+
+def demo_func() -> tuple:
+    return '', []
+
+with gr.Blocks() as demo:
+    chatbot = gr.components.Chatbot(label='Deloitte Audit Assistant')
+    msg = gr.components.Text(label='Input query')
+    clear = gr.components.Button(value='Clear', variant='stop')
+
+    msg.submit(
+        fn=set_user_query,
+        inputs=[msg, chatbot],
+        outputs=[msg, chatbot]
+    ).then(
+        fn=generate_response,
+        inputs=[msg, chatbot],
+        outputs=[chatbot]
+    )
+
+    clear.click(fn=demo_func, inputs=[], outputs=[msg, chatbot])
+
+demo.queue()
---
+++ CMPE273/Assignment-7/deloitte-chat-bot/untils.py
@@ -0,0 +1,70 @@
+import os
+
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def chat_completion(messages: list) -> list[str]:
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages,
+            stream=True
+        )
+        collected_messages = []
+        for chunk in response:
+            delta = chunk['choices'][0]['delta']
+            if 'content' in delta.keys():
+                collected_messages.append(delta['content'])
+        return collected_messages
+    except:
+        return ['We are facing a technical issue at this moment.']
+
+# print(chat_completion([
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": "Hello!"}
+#   ]))
+
+
+def format_messages(chat_history: list[list]) -> list[dict]:
+    formated_messages = [
+        {"role": "system", "content": "You are a helpful assistant who only answers tax related queries."}
+    ]
+    for i in range(len(chat_history)):
+        ch = chat_history[i]
+        formated_messages.append(
+            {
+                "role": "user",
+                "content": ch[0]
+            }
+        )
+        if ch[1] != None:
+            formated_messages.append(
+                {
+                    "role": "assistant",
+                    "content": ch[1]
+                }
+            )
+    return formated_messages
+
+chat_history = [['hi', None] ]
+
+print(format_messages(chat_history))
+
+def generate_response(text: str, chatbot: list[list]) -> tuple:
+    formated_messages = format_messages(chatbot)
+    bot_messages = chat_completion(formated_messages)
+    chatbot[-1][1] = ''
+    chatbot[-1][1] = ''
+    for bm in bot_messages:
+        chatbot[-1][1] += bm
+        yield chatbot
+    return chatbot
+
+def set_user_query(text: str, chatbot: list[list]) -> tuple:
+    chatbot += [[text, None]]
+    return '', chatbot
